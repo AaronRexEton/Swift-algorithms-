757f77c53ae505259320dddf6df10d54cc7d1b2c
Aaron Rex <Rex.A@etoncollege.org.uk>
2019-11-05 11:10:51 +0000

    Initial checking for code

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Rex, Aaron (SPH) on 05/11/2019.
+//  Copyright © 2019 Rex, Aaron (SPH). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Aaron Rex"
+print(name)
+//comments sgsdgsdsdadfs
